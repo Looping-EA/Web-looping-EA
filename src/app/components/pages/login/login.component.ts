770ba9a5d8a3b83020fbefc6f846cfdae72f4c8d
luis-domingo <luisdomingo.18@gmail.com>
2021-04-10 14:06:58 +0200

    Login | Falta la parte de autenticacion del usuario

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+    // Inputs, IMPORTANT!!! type of input must be declared @ the html <input type="----"> 
+    @ViewChild('usernameinput', {static: true}) usernameInput: ElementRef | undefined;
+    @ViewChild('passwordinput', {static: true}) passwordInput: ElementRef | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  loginOnClick(): void{
+
+    //GRAB THE FIELDS
+    const username = this.usernameInput?.nativeElement.value;
+    const pswrd = this.passwordInput?.nativeElement.value;
+
+    /*
+    MUST CHECK FORMATS:
+      - No empty strings
+      - No spaces in the strings
+          */
+
+    //EMPTY CHECK 
+    const check = 0;
+    for(let i = 0; i < username.length; i++){
+      if(username.charAt(i) == " "){
+        alert('Fill the blanks properly');
+      return;
+      }
+    }
+
+    //SPACE CHECK
+    for(let i = 0; i < pswrd.length; i++){
+      if(pswrd.charAt(i) == " "){
+        alert('Fill the blanks properly');
+      return;
+      }
+    }
+    if(username == "" || pswrd == ""){
+      alert('Fill the blanks properly');
+      return;
+    }
+
+    //SEND THE USER TO THE API
+
+    //MATCH THE USER
+    
+
+
+  }
+
 }
+
+ 
