110bef2285456f81b0d3d2474a5d24adf13be820
as43z <albertsigmaeng@gmail.com>
2021-04-21 13:23:07 +0200

    Register | workaround

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -27,56 +27,47 @@ export class RegisterComponent implements OnInit {
    
   
   ngOnInit(): void {
-    this.registerForm = this.FormBuilder.group({
-      usernameinput: new FormControl(''),
-      emailinput: new FormControl(''),
-      fullnameinput: new FormControl(''),
-      passwordinput: new FormControl('')
-    })
   }
 
   registerOnClick() : void {
-    const username=new FormControl('', Validators.required);
-    const email=new FormControl('', Validators.required);
-    const fullname=new FormControl('', Validators.required);
-    const password=new FormControl('', Validators.required);
+    this.registerForm = this.FormBuilder.group({
+      usernameinput: new FormControl('', Validators.required),
+      emailinput: new FormControl('', Validators.required),
+      fullnameinput: new FormControl('', Validators.required),
+      passwordinput: new FormControl('', Validators.required)
+    })
+    
     if (this.registerForm.valid){
-
       // GRAB THE FIELDS
       const usernameValue = this.usernameInput?.nativeElement.value;
       const emailValue = this.emailInput?.nativeElement.value;
       const fullnameValue = this.fullnameInput?.nativeElement.value;
       const pswrdValue = this.passwordInput?.nativeElement.value;
-    
-
       /*
-      
       MUST CHECK FORMATS:
         - No empty strings
         - Email must have email-ish sense
         - length of the fields must be over a certain limit
-      
       */
 
       // empty check
-      /*if(username == "" || email == "" || fullname == "" || pswrd == ""
-      || username == " " || email == " " || fullname == " " || pswrd == " "){
+      if(usernameValue.contains(' ') || pswrdValue.contains(' ')){
         alert('NO FIELD MUST BE LEFT BLANK');
         return;
       }
 
       // length check
-      if(username.length < 5 || pswrd.length < 4){
+      if(usernameValue.length < 5 || pswrdValue.length < 4){
         alert('SOME FIELDS DO NOT MATCH THE REQUIREMENTS');
         return;
       }
 
       // email validator
       const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
+      if(!re.test(emailValue)){
         alert('EMAIL HAS NO RIGHT FORMAT');
         return;
-      }*/
+      }
     
       // all successful, send the user to the API
       // GENERATE USER
@@ -92,11 +83,12 @@ export class RegisterComponent implements OnInit {
           alert(`User registered: ${response.uname}`);
         },
         (error) =>{
-          alert(`11`);
+          alert(`User was not registered`);
         }
       );
     }
-    else 
-    console.log("mal");
+    else { 
+      console.log("mal"); 
+    }
   }
 }
