4f3197ff9aadffab1ac4f810fd401bd2773cf50b
MaFe1401 <marcferre42@gmail.com>
2021-04-27 23:28:36 +0200

    soy el admin

--- src/app/components/pages/admin/admin.component.ts
+++ src/app/components/pages/admin/admin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { User } from './../../../shared/interfaces/user.interface';
+import { UserService } from './../../../shared/services/user.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common'
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(@Inject(DOCUMENT) private document: Document, private userService: UserService, private route: ActivatedRoute, private router: Router) { }
+  users:User[]=[];
   ngOnInit(): void {
+    this.receiveUsers();
+  }
+  receiveUsers(): void{
+    this.userService.getAllUsers().subscribe(
+      (response)=>{this.users=response},
+      (error)=>{console.log("no users")}
+    );
+  }
+  onDeleteClick(uname: string){
+    
+    this.userService.deleteUser(uname).subscribe(
+      (response)=>{window.location.reload();},
+      (error)=>{console.log("error")}
+    );
   }
 
 }
