fbeed0e8e82d7f458f54061f5b074206c06cd59e
MaFe1401 <marcferre42@gmail.com>
2021-04-28 00:10:55 +0200

    login fixed

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from './../../../shared/interfaces/user.interface';
 import { UserService } from './../../../shared/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,15 +15,17 @@ export class LoginComponent implements OnInit {
   @ViewChild('usernameinput', {static: true}) usernameInput: ElementRef | undefined;
   @ViewChild('passwordinput', {static: true}) passwordInput: ElementRef | undefined;
 
-  loginForm=new FormGroup({
-    usernameinput: new FormControl('', [Validators.required]),
-    passwordinput: new FormControl('', [Validators.required]),
-  })
+  loginForm=new FormGroup({});
 
-  constructor(private UserService: UserService, private FormBuilder: FormBuilder) { }
+  constructor(private UserService: UserService, private formBuilder: FormBuilder, private route: Router) {
+   
+   }
+   get formControls(){
+     return this.loginForm.controls;
+   }
 
   ngOnInit(): void {
-    this.loginForm = this.FormBuilder.group({
+    this.loginForm = this.formBuilder.group({
       usernameinput: new FormControl('', [Validators.required, Validators.minLength(5), this.noSpaces]),
       passwordinput: new FormControl('', [Validators.required, Validators.minLength(8), this.noSpaces])
     })
@@ -33,30 +36,22 @@ export class LoginComponent implements OnInit {
     if(this.loginForm.valid){
 
       //GRAB THE FIELDS
-      const username = this.usernameInput?.nativeElement.value;
-      const pswrd = this.passwordInput?.nativeElement.value;
+      const username = this.loginForm.value.usernameinput;
+      const pswrd = this.loginForm.value.passwordinput;
 
-      /*
-      MUST CHECK FORMATS:
-        - No empty strings
-            */
-
-      //EMPTY CHECK 
-      if(username.includes(' ') || pswrd.includes(' ')){
-        alert('NO FIELD MUST BE LEFT BLANK');
-        return;
-      }
+      
 
       //SEND THE USER TO THE API
 
       const user = {
-        "uname": username,
-        "pswd": pswrd
+        uname: username,
+        pswd: pswrd
       } as User;
 
       this.UserService.loginUser(user).subscribe(
         (response) =>{
           alert(`${response.uname} welcome`);
+          this.route.navigate(['/home']);
         },
         (error) =>{
           alert(`Try again`);
